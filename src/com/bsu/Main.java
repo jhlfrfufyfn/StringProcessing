fc02b1a115feee30f2a6eade5265068269cea622
jhlfrfufyfn <jhlfrfufyfn@gmail.com>
2020-09-16 19:24:19 +0300

    Added code and tests

--- src/com/bsu/Main.java
+++ src/com/bsu/Main.java
@@ -5,19 +5,20 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
+        try (Scanner cin = new Scanner(System.in)) {
+            StringBuilder textBuilder = new StringBuilder();
 
-        Scanner cin = new Scanner(System.in);
+            System.out.println("Enter the text: ");
+            String line;
+            while (!(line = cin.nextLine()).isEmpty()) {
+                textBuilder.append(line).append('\n');
+            }
 
-        StringBuilder textBuilder = new StringBuilder();
+            String text = StringProcessor.deleteNumbers(textBuilder.toString());
 
-        System.out.println("Enter the text: ");
-        String line;
-        while (!(line = cin.nextLine()).isEmpty()) {
-            textBuilder.append(line).append('\n');
+            System.out.println('\'' + text + '\'');
+        } catch (Exception ex) {
+            System.out.println("Error while reading: " + ex);
         }
-
-        String text = StringProcessor.deleteNumbers(new String(textBuilder));
-
-        System.out.println('\'' + text + '\'');
     }
 }
